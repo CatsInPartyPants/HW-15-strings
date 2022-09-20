b03575b6c0e3c03e1e4de90cb8995a0cb06457a8
Artem Nilov <nilov1235@mail.ru>
2022-09-20 12:51:29 +0300

    7

--- HW 15 strings/HW 15 strings.cpp
+++ HW 15 strings/HW 15 strings.cpp
@@ -19,6 +19,18 @@ void char_input(char*, char, int);
 
 void change_points(char* arr, int size);
 
+//5
+
+int number_of_simbols(char* arr, char simbol, int size);
+
+//6
+
+void analyze_array(char* arr);
+
+//7
+
+void change_spaces_to_tabs(char* arr);
+
 int main()
 {
     //1
@@ -49,6 +61,31 @@ int main()
     arr4[255] = '\0';
     change_points(arr4, 256);
     cout << arr4 << endl;
+    delete[] arr4;
+
+    //5
+    char* arr5 = new char[256];
+    char simbol_for_find;
+    cout << "Enter string:\n";
+    gets_s(arr5, 256);
+    cout << "Simbol for find:\n";
+    cin >> simbol_for_find;
+    cout << "Simbol " << simbol_for_find << " is " << number_of_simbols(arr5, simbol_for_find, 256) << " times in the string!\n";
+    delete[] arr5;
+
+    //6
+    char* arr6 = new char[] {"aaa123rsd2s3a !a%l6r%la4relaa a a a!"};
+    analyze_array(arr6);
+    delete[] arr6;
+
+    //7 
+    
+    char* arr7 = new char[] {"aaa123rsd2s3a !a%l6r%la4relaa a a a!"};
+    cout << arr7 << endl;
+    change_spaces_to_tabs(arr7);
+    cout << arr7 << endl;
+    delete[] arr7;
+
 }
 
 //1
@@ -96,5 +133,56 @@ void change_points(char* arr, int size)
     {
         if (*(arr+i) == '.')
             *(arr+i) = '!';
+    }
+}
+
+//5
+
+int number_of_simbols(char* arr, char simbol, int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (*(arr + i) == simbol)
+            count++;
+    }
+    return count;
+}
+
+//6
+
+void analyze_array(char* arr)
+{
+    int number_count = 0;
+    int letter_count = 0;
+    int other_count = 0;
+    for (int i = 0; i < strlen(arr); i++)
+    {
+        if (isdigit(*(arr + i)) != 0)
+        {
+            number_count++;
+        }
+        else if (isalpha(*(arr + i)) != 0)
+        {
+            letter_count++;
+        }
+        else if (*(arr + i) != ' ')
+        {
+            other_count++;
+        }
+    }
+    cout << "Numbers in the string: " << number_count << " Letters in the string: " << letter_count << " other simbols in the string " << other_count << endl;
+}
+
+//7 
+void change_spaces_to_tabs(char* arr)
+{
+    while(*arr != '\0')
+    {
+        if (*arr == ' ')
+        {
+            *arr == '\t';
+        }
+        arr++;
     }
 }
